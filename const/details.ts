aa872246f793fffd86d453301e72408a85ccde11
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-19 13:13:57 +0000

    Update details.ts

--- const/details.ts
+++ const/details.ts
@@ -3,4 +3,4 @@ import CustomChain from "../const/chain";
 export const TOKEN_ADDRESS: string =
   "0x9b84D0E4fc807CB664aFdBF9E7096bed3a7aB18A";
 export const DEX_ADDRESS: string = "0x92c85c31c74F482AB30D94D7e5c63fe9BD53883B";
-export const ACTIVE_CHAIN: Chain = Mumbai;
+export const NETWORK = CustomChain;
