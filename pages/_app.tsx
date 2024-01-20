ce443295d0e3785bb022d91ed7e2970f6e7b980f
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-20 01:49:02 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import CustomChain from "../const/chain";
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import type { NextComponentType } from "next";
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp: NextComponentType = ({ Component, pageProps }) => {
   return (
     <ThirdwebProvider
       activeChain={CustomChain}
