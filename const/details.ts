24621fe5b857b8f749361a1aa00493e78902c83d
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-21 16:06:01 +0000

    Update details.ts

--- const/details.ts
+++ const/details.ts
@@ -2,5 +2,5 @@ import CustomChain from "../const/chain";
 
 export const TOKEN_ADDRESS: string =
   "0x82fe97150B42286889Dc3f3c8521973D5c0984EE";
-export const DEX_ADDRESS: string = "0xf63304f9Dd20d096e034e6644E6f7bED39E784a3";
+export const DEX_ADDRESS: string = "0x0E657945e15E240D1a0944bc9d700cCDEc3EFD32";
 export const NETWORK = CustomChain;
